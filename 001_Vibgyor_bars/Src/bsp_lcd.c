973035b16df358841d030dba8042a18e6608aba7
Sayan <rana.sayan@gmail.com>
2024-09-18 01:25:22 +0530

    LCD command code written in bsp_lcd.c

--- 001_Vibgyor_bars/Src/bsp_lcd.c
+++ 001_Vibgyor_bars/Src/bsp_lcd.c
@@ -7,6 +7,17 @@
 
 #include "stm32f429xx.h"
 #include "reg_util.h"
+#include "ili9341_reg.h"
+
+
+
+#define MADCTL_MY 0x80  ///< Bottom to top
+#define MADCTL_MX 0x40  ///< Right to left
+#define MADCTL_MV 0x20  ///< Reverse Mode
+#define MADCTL_ML 0x10  ///< LCD refresh Bottom to top
+#define MADCTL_RGB 0x00 ///< Red-Green-Blue pixel order
+#define MADCTL_BGR 0x08 ///< Blue-Green-Red pixel order
+#define MADCTL_MH 0x04  ///< LCD refresh right to left
 
 
 #define GPIO_PIN_0			0U
@@ -25,14 +36,6 @@
 #define GPIO_PIN_13			13U
 #define GPIO_PIN_14			14U
 #define GPIO_PIN_15			15U
-
-
-/* Static helper functions */
-static void LCD_Pin_Init(void);
-static void LCD_SPI_Init(void);
-static void LCD_Reset(void);
-static void LCD_Config(void);
-static void LCD_SPI_Enable(void);
 
 
 /* Defining all the LCD signals */
@@ -65,6 +68,31 @@ static void LCD_SPI_Enable(void);
 	#error "Supported device is not selected"
 #endif
 
+
+/* LCD RESX pin high and low macro */
+#define LCD_RESX_HIGH()                 (REG_SET_BIT(LCD_RESX_PORT->ODR, GPIO_ODR_OD7_Pos))
+#define LCD_RESX_LOW()                  (REG_CLR_BIT(LCD_RESX_PORT->ODR, GPIO_ODR_OD7_Pos))
+
+
+/* LCD CSX pin high and low macro */
+#define LCD_CSX_HIGH()                  (REG_SET_BIT(LCD_CSX_PORT->ODR, GPIO_ODR_OD2_Pos))
+#define LCD_CSX_LOW()                   (REG_CLR_BIT(LCD_CSX_PORT->ODR, GPIO_ODR_OD2_Pos))
+
+
+/* LCD DCX pin high and low macro */
+#define LCD_DCX_HIGH()                  (REG_SET_BIT(LCD_WRX_DCX_PORT->ODR, GPIO_ODR_OD13_Pos))
+#define LCD_DCX_LOW()                   (REG_CLR_BIT(LCD_WRX_DCX_PORT->ODR, GPIO_ODR_OD13_Pos))
+
+
+/* Static helper functions */
+static void LCD_Pin_Init(void);
+static void LCD_SPI_Init(void);
+static void LCD_Reset(void);
+static void LCD_Config(void);
+static void LCD_SPI_Enable(void);
+static void LCD_Write_Cmd(uint8_t cmd);
+static void LCD_Write_Data(uint8_t *pData, uint32_t len);
+void delay_50ms(void);
 
 
 void BSP_LCD_Init(void) {
@@ -138,6 +166,7 @@ void LCD_SPI_Init(void) {
 
 	/* Configure SPI as master in half-duplex MOTOROLA mode */
 	REG_SET_BIT(pSPI->CR1, SPI_CR1_BIDIMODE_Pos);							// BIDI mode enable(half-duplex)
+	REG_SET_BIT(pSPI->CR1, SPI_CR1_BIDIOE_Pos);                             // In BIDI mode transmit only mode is selected
 	REG_CLR_BIT(pSPI->CR1, SPI_CR1_DFF_Pos);								// Data frame format 8 bit is selected
 	REG_SET_BIT(pSPI->CR1, SPI_CR1_SSM_Pos);								// Enabling software slave management
 	REG_SET_BIT(pSPI->CR1, SPI_CR1_SSI_Pos);								// Setting NSS pin high manually
@@ -152,23 +181,209 @@ void LCD_SPI_Init(void) {
 
 
 void LCD_Reset(void) {
-
+	/* Creating a 50mS reset pulse */
+	LCD_RESX_LOW();
+	delay_50ms();
+	LCD_RESX_HIGH();
+	delay_50ms();
 }
 
 
 
 void LCD_Config(void) {
+	uint8_t params[15] = {0};
+	LCD_Write_Cmd(ILI9341_SWRESET);
+	LCD_Write_Cmd(ILI9341_POWERB);
+	params[0] = 0x00;
+	params[1] = 0xD9;
+	params[2] = 0x30;
+	LCD_Write_Data(params, 3);
 
+	LCD_Write_Cmd(ILI9341_POWER_SEQ);
+	params[0]= 0x64;
+	params[1]= 0x03;
+	params[2]= 0X12;
+	params[3]= 0X81;
+	LCD_Write_Data(params, 4);
+
+	LCD_Write_Cmd(ILI9341_DTCA);
+	params[0]= 0x85;
+	params[1]= 0x10;
+	params[2]= 0x7A;
+	LCD_Write_Data(params, 3);
+
+	LCD_Write_Cmd(ILI9341_POWERA);
+	params[0]= 0x39;
+	params[1]= 0x2C;
+	params[2]= 0x00;
+	params[3]= 0x34;
+	params[4]= 0x02;
+	LCD_Write_Data(params, 5);
+
+	LCD_Write_Cmd(ILI9341_PRC);
+	params[0]= 0x20;
+	LCD_Write_Data(params, 1);
+
+	LCD_Write_Cmd(ILI9341_DTCB);
+	params[0]= 0x00;
+	params[1]= 0x00;
+	LCD_Write_Data(params, 2);
+
+	LCD_Write_Cmd(ILI9341_POWER1);
+	params[0]= 0x1B;
+	LCD_Write_Data(params, 1);
+
+	LCD_Write_Cmd(ILI9341_POWER2);
+	params[0]= 0x12;
+	LCD_Write_Data(params, 1);
+
+	LCD_Write_Cmd(ILI9341_VCOM1);
+	params[0]= 0x08;
+	params[1]= 0x26;
+	LCD_Write_Data(params, 2);
+
+	LCD_Write_Cmd(ILI9341_VCOM2);
+	params[0]= 0XB7;
+	LCD_Write_Data(params, 1);
+
+	uint8_t m;
+	m = MADCTL_MV | MADCTL_MY| MADCTL_BGR;
+
+	LCD_Write_Cmd(ILI9341_MAC);    // Memory Access Control <Landscape setting>
+	params[0]= m;
+	LCD_Write_Data(params, 1);
+
+	LCD_Write_Cmd(ILI9341_PIXEL_FORMAT);
+	params[0]= 0x55; //select RGB565
+	LCD_Write_Data(params, 1);
+
+	LCD_Write_Cmd(ILI9341_FRMCTR1);
+	params[0]= 0x00;
+	params[1]= 0x1B;//frame rate = 70
+	LCD_Write_Data(params, 2);
+
+	LCD_Write_Cmd(ILI9341_DFC);    // Display Function Control
+	params[0]= 0x0A;
+	params[1]= 0xA2;
+	LCD_Write_Data(params, 2);
+
+	LCD_Write_Cmd(ILI9341_3GAMMA_EN);    // 3Gamma Function Disable
+	params[0]= 0x02;
+	LCD_Write_Data(params, 1);
+
+	LCD_Write_Cmd(ILI9341_GAMMA);
+	params[0]= 0x01;
+	LCD_Write_Data(params, 1);
+
+	LCD_Write_Cmd(ILI9341_PGAMMA);    //Set Gamma
+	params[0]= 0x0F;
+	params[1]= 0x1D;
+	params[2]= 0x1A;
+	params[3]= 0x0A;
+	params[4]= 0x0D;
+	params[5]= 0x07;
+	params[6]= 0x49;
+	params[7]= 0X66;
+	params[8]= 0x3B;
+	params[9]= 0x07;
+	params[10]= 0x11;
+	params[11]= 0x01;
+	params[12]= 0x09;
+	params[13]= 0x05;
+	params[14]= 0x04;
+	LCD_Write_Data(params, 15);
+
+	LCD_Write_Cmd(ILI9341_NGAMMA);
+	params[0]= 0x00;
+	params[1]= 0x18;
+	params[2]= 0x1D;
+	params[3]= 0x02;
+	params[4]= 0x0F;
+	params[5]= 0x04;
+	params[6]= 0x36;
+	params[7]= 0x13;
+	params[8]= 0x4C;
+	params[9]= 0x07;
+	params[10]= 0x13;
+	params[11]= 0x0F;
+	params[12]= 0x2E;
+	params[13]= 0x2F;
+	params[14]= 0x05;
+	LCD_Write_Data(params, 15);
+
+	LCD_Write_Cmd(ILI9341_RASET); //page address set
+	params[0]= 0x00;
+	params[1]= 0x00;
+	params[2]= 0x00;
+	params[3]= 0xf0; //240 rows = 0xf0
+	LCD_Write_Data(params, 4);
+
+	LCD_Write_Cmd(ILI9341_CASET);
+	params[0]= 0x00;
+	params[1]= 0x00;
+	params[2]= 0x01;
+	params[3]= 0x40; //320 columns = 0x140
+	LCD_Write_Data(params, 4);
+
+	LCD_Write_Cmd(ILI9341_RGB_INTERFACE);
+	params[0] = 0xC2; //Data is fetched during falling edge of DOTCLK
+	LCD_Write_Data(params, 1);
+
+	LCD_Write_Cmd(ILI9341_INTERFACE);
+	params[0] = 0x00;
+	params[1] = 0x00;
+	params[2] = 0x06;
+	LCD_Write_Data(params, 3);
+
+	LCD_Write_Cmd(ILI9341_SLEEP_OUT); //Exit Sleep
+	delay_50ms();
+	delay_50ms();
+	LCD_Write_Cmd(ILI9341_DISPLAY_ON); //display on
 }
 
 
 
-void LCD_SPI_Enable(void)
-{
+void LCD_SPI_Enable(void){
 	SPI_TypeDef* pSPI = SPI;
 	REG_SET_BIT(pSPI->CR1, SPI_CR1_SPE_Pos);								// SPI enable
 }
 
 
 
+void LCD_Write_Cmd(uint8_t cmd) {
+	SPI_TypeDef *pSPI = SPI;
+	LCD_CSX_LOW();                                                          // CSX pin LOW to initiate SPI communication
+	LCD_DCX_LOW();                                                          // DCX pin LOW means command
+	while(!REG_READ_BIT(pSPI->SR, SPI_SR_TXE_Pos));                         // Wait until TXE = 1 (Tx buffer is empty)
+	REG_WRITE(pSPI->DR, cmd);                                               // Transmitting the command
 
+	// Wait until TXE is 1 (Transmit buffer is empty) and BSY is 0 (SPI is not busy)
+	while((!REG_READ_BIT(pSPI->SR, SPI_SR_TXE_Pos)) || (REG_READ_BIT(pSPI->SR, SPI_SR_BSY_Pos)));
+	REG_CLR_BIT(pSPI->CR1, SPI_CR1_SPE_Pos);                                // Disabling the SPI peripheral
+
+	LCD_DCX_HIGH();                                                         // After transfer setting DCX pin HIGH
+	LCD_CSX_HIGH();                                                         // After transfer setting CSX pin HIGH
+}
+
+
+
+void LCD_Write_Data(uint8_t *pData, uint32_t len) {
+	SPI_TypeDef *pSPI = SPI;
+	LCD_CSX_LOW();                                                          // CSX pin LOW to initiate data transfer
+	for(uint32_t i = 0; i < len; i++) {
+		while(!REG_READ_BIT(pSPI->SR, SPI_SR_TXE_Pos));                     // Wait until TXE = 1 (Tx buffer is empty)
+		REG_WRITE(pSPI->DR, pData[i]);                                      // Transmitting command
+	}
+
+	// Wait until TXE is 1 (Transmit buffer is empty) and BSY is 0 (SPI is not busy)
+	while((!REG_READ_BIT(pSPI->SR, SPI_SR_TXE_Pos)) || (REG_READ_BIT(pSPI->SR, SPI_SR_BSY_Pos)));
+	REG_CLR_BIT(pSPI->CR1, SPI_CR1_SPE_Pos);                                // Disabling the SPI peripheral
+	LCD_CSX_HIGH();                                                         // After transfer setting CSX pin HIGH
+}
+
+
+
+/* A software delay of 50ms */
+void delay_50ms(void) {
+	for(uint32_t i = 0; i < (0xFFFFU * 10U); i++);
+}
